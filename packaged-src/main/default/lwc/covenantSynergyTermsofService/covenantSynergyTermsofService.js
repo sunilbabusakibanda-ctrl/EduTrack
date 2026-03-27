bc5cf53863327c3ef881917875ae51408a7d05a6
Your NameSunilbabu1432 <you@example.com>
2026-03-28 01:56:20 +0530

    Fix missing @AuraEnabled in ClassManagementController and improve teacher queries

---
+++ packaged-src/main/default/lwc/covenantSynergyTermsofService/covenantSynergyTermsofService.js
@@ -0,0 +1,6 @@
+import { LightningElement, api } from 'lwc';
+
+export default class CovenantSynergyTermsOfService extends LightningElement {
+    @api cardTitle = 'Terms of Service for Covenant Synergy';
+    @api cardIcon = 'standard:document';
+}
